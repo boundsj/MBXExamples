e5b45ba80bf679d7198a5d2479f4978ffeb60f11
Jesse Bounds <jesse@rebounds.net>
2017-03-16 07:20:18 -0700

    wip add new layer for each new shape / transition

--- MBXExamples/QueryShapesActivity.swift
+++ MBXExamples/QueryShapesActivity.swift
@@ -1,8 +1,12 @@
 import Mapbox
 
-class CustomFeature: MGLPointFeature {}
-
 class QueryShapesActivity: NSObject, MapboxMapActivity {
+
+    var shapeSource: MGLShapeSource!
+    var circleStyleLayer: MGLCircleStyleLayer!
+    var newCircleStyleLayer: MGLCircleStyleLayer?
+
+    var isStyleLoaded: Bool = false
 
     let centerCoordinate = CLLocationCoordinate2DMake(22.286394, 114.149139)
 
@@ -15,30 +19,77 @@ class QueryShapesActivity: NSObject, MapboxMapActivity {
 
 extension QueryShapesActivity: MGLMapViewDelegate {
 
-    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
-        let feature = CustomFeature()
+    func generateFeature(centerCoordinate: CLLocationCoordinate2D) -> MGLPointFeature {
+        let feature = MGLPointFeature()
         feature.coordinate = centerCoordinate
-        feature.attributes = ["hexColor": "#00ff00"]
+        feature.attributes = ["hexColor": "#ff00ff"]
+        feature.identifier = 42
+        feature.title = "demo"
 
-        let shapeSource = MGLShapeSource(identifier: "shape-source", features: [feature], options: nil)
+        return feature
+    }
+
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        isStyleLoaded = true
+
+        let feature = generateFeature(centerCoordinate: mapView.centerCoordinate)
+        feature.attributes = ["status": "old"]
+        let featureCollection = MGLShapeCollectionFeature(shapes: [feature])
+
+        shapeSource = MGLShapeSource(identifier: "shape-source", shape: featureCollection, options: nil)
         style.addSource(shapeSource)
 
-        let circleStyleLayer = MGLCircleStyleLayer(identifier: "circle-layer", source: shapeSource)
+        circleStyleLayer = MGLCircleStyleLayer(identifier: "circle-layer", source: shapeSource)
+        circleStyleLayer.predicate = NSPredicate(format: "status == 'old'")
         circleStyleLayer.circleColor = MGLStyleValue(
             interpolationMode: .identity,
             sourceStops: nil,
             attributeName: "hexColor",
             options: [.defaultValue: MGLStyleValue<UIColor>(rawValue: .red)]
         )
-        circleStyleLayer.circleRadius = MGLStyleValue<NSNumber>(rawValue: 15)
+        circleStyleLayer.circleRadius = MGLStyleValue<NSNumber>(rawValue: 25)
         style.addLayer(circleStyleLayer)
+    }
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0)  {
-            let point = mapView.convert(self.centerCoordinate, toPointTo: mapView)
-            let features = mapView.visibleFeatures(at: point)
-            let roundTripFeature = features.filter { return $0 === feature; }
-            print(roundTripFeature)
+    func mapView(_ mapView: MGLMapView, regionDidChangeAnimated animated: Bool) {
+        guard isStyleLoaded else {
+            return
         }
+
+        // Add a new feature
+        let feature = generateFeature(centerCoordinate: mapView.centerCoordinate)
+        feature.attributes = ["status": "new"]
+        let newFeatures = [feature]
+
+        // Get all of the old features (as shapes), cast them to features, update their
+        // attributes, and collect them again casted back to shape. Finally 
+        // create a new shape colection to add back as the shape to the source
+        let shapeCollectionFeature = shapeSource.shape as! MGLShapeCollectionFeature
+        let shapes = shapeCollectionFeature.shapes.map { $0 as! MGLFeature }
+        let existingFeatures: [MGLShape] = shapes.map { (shape: MGLFeature) -> MGLShape in
+            shape.attributes = ["status": "old"]
+            return shape as! MGLShape
+        }
+        let allFeatures = newFeatures + existingFeatures as [MGLShape]
+        let shapeCollection = MGLShapeCollectionFeature(shapes: allFeatures)
+        shapeSource.shape = shapeCollection
+
+        // Remove any previous style layer used for adding the new shape
+        if let newCircleStyleLayer = newCircleStyleLayer {
+            mapView.style?.removeLayer(newCircleStyleLayer)
+        }
+
+        // Add a new style layer so that new features show a transition
+        newCircleStyleLayer = MGLCircleStyleLayer(identifier: "new-circle-layer", source: shapeSource)
+        newCircleStyleLayer?.circleColor = MGLStyleValue(
+            interpolationMode: .identity,
+            sourceStops: nil,
+            attributeName: "hexColor",
+            options: [.defaultValue: MGLStyleValue<UIColor>(rawValue: .red)]
+        )
+        newCircleStyleLayer?.circleRadiusTransition = MGLTransition(duration: 2, delay: 0)
+        newCircleStyleLayer?.circleRadius = MGLStyleValue<NSNumber>(rawValue: 25)
+        mapView.style?.addLayer(newCircleStyleLayer!)
     }
 
 }

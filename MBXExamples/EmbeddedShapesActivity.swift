186e3117e4630ba69c1aa28ab201c3f0985a871d
Jesse Bounds <jesse@rebounds.net>
2017-08-25 10:53:51 -0700

    Update to 3.6.2
    Also make embedded shapes activity simply update a symbol layer

--- MBXExamples/EmbeddedShapesActivity.swift
+++ MBXExamples/EmbeddedShapesActivity.swift
@@ -2,7 +2,7 @@ import Mapbox
 
 class EmbeddedShapesActivity: NSObject, MapboxMapActivity {
 
-    let centerCoordinate = CLLocationCoordinate2DMake(39.5720821, -84.367218)
+    let centerCoordinate = CLLocationCoordinate2DMake(39.5820821, -84.367218)
 
     func update(mapView: MGLMapView) {
         mapView.styleURL = MGLStyle.outdoorsStyleURL(withVersion: 9)
@@ -17,21 +17,34 @@ extension EmbeddedShapesActivity: MGLMapViewDelegate {
     func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
 
         let point = MGLPointFeature()
-        let hotSpot = CLLocationCoordinate2DMake(39.5718, -84.36742)
-        point.coordinate = hotSpot
+        point.coordinate = CLLocationCoordinate2DMake(39.5820821, -84.367218)
+        point.attributes = ["bike_count": 95]  // Assume a station can have a max of 100 bikes
 
-        let source = MGLShapeSource(identifier: "test", features: [point], options: nil)
+        let source = MGLShapeSource(identifier: "shapeSource", shape: point, options: nil)
         style.addSource(source)
 
-        let circleStyleLayer = MGLCircleStyleLayer(identifier: "test", source: source)
-        circleStyleLayer.circleColor = MGLStyleValue(rawValue: .red)
-        circleStyleLayer.circleRadius = MGLStyleValue(rawValue: 50)
-        circleStyleLayer.circleBlur = MGLStyleValue(rawValue: 1)
-
+        // Represent the bike stop with a circle that is red as the bike_count approaches 0 and green as the bike_count approaches 100
+        let circleStyleLayer = MGLCircleStyleLayer(identifier: "circleLayer", source: source)
+        let stops : [Float:MGLStyleValue<UIColor>] = [
+            0: MGLStyleValue<UIColor>(rawValue: .red),
+            100: MGLStyleValue<UIColor>(rawValue: .green)
+        ]
+        circleStyleLayer.circleColor = MGLStyleValue(interpolationMode: .exponential, sourceStops: stops, attributeName: "bike_count", options: nil)
+        circleStyleLayer.circleRadius = MGLStyleValue(rawValue: 18)
         style.addLayer(circleStyleLayer)
-
-        for layer in style.layers {
-            print(layer.identifier)
+        
+        // Add a label on top of the circle that displays the actual bike_count value
+        let symbolStyleLayer = MGLSymbolStyleLayer(identifier: "symbolLayer", source: source)
+        symbolStyleLayer.text = MGLStyleValue(rawValue: "{bike_count}")
+        symbolStyleLayer.textColor = MGLStyleValue(rawValue: .white)
+        style.addLayer(symbolStyleLayer)
+        
+        // After 5 seconds reduce the number of bikes from 95 to 10
+        // Note: This simulates an update that could happen on a timer. You will need to save a reference to the source
+        // or get it back from the style using `mapView.style?.source(withIdentifier:)`
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            point.attributes = ["bike_count": 10]
+            source.shape = point
         }
     }
 
--- Podfile
+++ Podfile
@@ -2,6 +2,6 @@ platform :ios, '9.0'
 
 target 'MBXExamples' do
   #pod 'Mapbox-iOS-SDK-symbols', :path => '../../mapbox-gl-native/build/ios/pkg/dynamic/Mapbox-iOS-SDK-symbols.podspec'
-  pod "Mapbox-iOS-SDK-symbols", :podspec => 'https://raw.githubusercontent.com/mapbox/mapbox-gl-native/ios-v3.6.0-beta.1/platform/ios/Mapbox-iOS-SDK-symbols.podspec'
-  #pod 'Mapbox-iOS-SDK', '3.5.4'
+  #pod "Mapbox-iOS-SDK-symbols", :podspec => 'https://raw.githubusercontent.com/mapbox/mapbox-gl-native/ios-v3.6.0-beta.1/platform/ios/Mapbox-iOS-SDK-symbols.podspec'
+  pod 'Mapbox-iOS-SDK', '3.6.2'
 end

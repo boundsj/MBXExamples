fb66fd247a7eaaeba852669d267f59d6374dad50
Jesse Bounds <jesse@rebounds.net>
2017-10-03 13:06:15 -0700

    Update to latest SDK version

--- MBXExamples/EmbeddedShapesActivity.swift
+++ MBXExamples/EmbeddedShapesActivity.swift
@@ -1,13 +1,17 @@
 import Mapbox
 
 class EmbeddedShapesActivity: NSObject, MapboxMapActivity {
+    
+    var source : MGLShapeSource!
+    var mapView: MGLMapView!
 
-    let centerCoordinate = CLLocationCoordinate2DMake(39.5820821, -84.367218)
+    let centerCoordinate = CLLocationCoordinate2D(latitude: 48.5, longitude: -73.2)
 
     func update(mapView: MGLMapView) {
         mapView.styleURL = MGLStyle.outdoorsStyleURL(withVersion: 9)
         mapView.delegate = self
         mapView.setCenter(centerCoordinate, zoomLevel: 17, animated: false)
+        self.mapView = mapView
     }
 
 }
@@ -16,36 +20,41 @@ extension EmbeddedShapesActivity: MGLMapViewDelegate {
 
     func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
 
-        let point = MGLPointFeature()
-        point.coordinate = CLLocationCoordinate2DMake(39.5820821, -84.367218)
-        point.attributes = ["bike_count": 95]  // Assume a station can have a max of 100 bikes
-
-        let source = MGLShapeSource(identifier: "shapeSource", shape: point, options: nil)
-        style.addSource(source)
-
-        // Represent the bike stop with a circle that is red as the bike_count approaches 0 and green as the bike_count approaches 100
-        let circleStyleLayer = MGLCircleStyleLayer(identifier: "circleLayer", source: source)
-        let stops : [Float:MGLStyleValue<UIColor>] = [
-            0: MGLStyleValue<UIColor>(rawValue: .red),
-            100: MGLStyleValue<UIColor>(rawValue: .green)
-        ]
-        circleStyleLayer.circleColor = MGLStyleValue(interpolationMode: .exponential, sourceStops: stops, attributeName: "bike_count", options: nil)
-        circleStyleLayer.circleRadius = MGLStyleValue(rawValue: 18)
-        style.addLayer(circleStyleLayer)
         
-        // Add a label on top of the circle that displays the actual bike_count value
-        let symbolStyleLayer = MGLSymbolStyleLayer(identifier: "symbolLayer", source: source)
-        symbolStyleLayer.text = MGLStyleValue(rawValue: "{bike_count}")
-        symbolStyleLayer.textColor = MGLStyleValue(rawValue: .white)
-        style.addLayer(symbolStyleLayer)
-        
-        // After 5 seconds reduce the number of bikes from 95 to 10
-        // Note: This simulates an update that could happen on a timer. You will need to save a reference to the source
-        // or get it back from the style using `mapView.style?.source(withIdentifier:)`
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            point.attributes = ["bike_count": 10]
-            source.shape = point
+        // So that the features are only added once.
+        if mapView.styleURL != MGLStyle.darkStyleURL(withVersion: 9) {
+            
+            var features = Array<MGLPointFeature>()
+            let point1 = MGLPointFeature()
+            point1.coordinate = CLLocationCoordinate2D(latitude: 48.5, longitude: -73.2)
+            features.append(point1)
+            
+            let point2 = MGLPointFeature()
+            point2.coordinate = CLLocationCoordinate2D(latitude: 35.8, longitude: -71.8)
+            features.append(point2)
+            
+            let shapeCollection = MGLShapeCollectionFeature(shapes: features)
+            source = MGLShapeSource(identifier:"circle-source", shape: shapeCollection, options: nil)
+            mapView.style?.addSource(source)
+            
+            let circleLayer = MGLCircleStyleLayer(identifier:"circle-layer", source: source)
+            mapView.style?.addLayer(circleLayer)
+                        
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: { 
+                self.removeFeatures()
+                mapView.styleURL = MGLStyle.darkStyleURL(withVersion: 9)
+            })
         }
+        
+        
+    }
+    
+    
+    func removeFeatures() {
+        let filtered = [MGLShape]()
+        let newShapeCollection = MGLShapeCollectionFeature(shapes: filtered)
+        
+        self.source?.shape = newShapeCollection
     }
 
 }
--- Podfile
+++ Podfile
@@ -3,5 +3,5 @@ platform :ios, '9.0'
 target 'MBXExamples' do
   #pod 'Mapbox-iOS-SDK-symbols', :path => '../../mapbox-gl-native/build/ios/pkg/dynamic/Mapbox-iOS-SDK-symbols.podspec'
   #pod "Mapbox-iOS-SDK-symbols", :podspec => 'https://raw.githubusercontent.com/mapbox/mapbox-gl-native/ios-v3.6.0-beta.1/platform/ios/Mapbox-iOS-SDK-symbols.podspec'
-  pod 'Mapbox-iOS-SDK', '3.6.2'
+  pod 'Mapbox-iOS-SDK', '3.6.4'
 end
